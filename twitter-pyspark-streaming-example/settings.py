cad30bda4a56e3e1ab0df91f45c9ccbb8a28b8b1
chie8842 <chie8842@gmail.com>
2017-09-24 13:04:37 +0900

    add settings.py

---
+++ twitter-pyspark-streaming-example/settings.py
@@ -0,0 +1,8 @@
+# consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+consumer_key = ""
+# consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+consumer_secret = ""
+# access_token = os.environ['TWITTER_ACCESS_TOKEN']
+access_token = ""
+# access_secret = os.environ['TWITTER_ACCESS_SECRET']
+access_secret = ""
