1fa55f0eb1a8891c2725731eee861ad59b009497
chie8842 <chie8842@gmail.com>
2017-09-24 13:03:48 +0900

    first commit

---
+++ twitter-pyspark-streaming-example/tweetsparkstreaming.py
@@ -0,0 +1,27 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+
+def main():
+    # SparkContext、StreamingContextの初期化
+    sc = SparkContext("local[2]", "TwitterStreaming")
+    ssc = StreamingContext(sc, 1)
+
+    # デモを見やすくするため、ログ出力を少なくする
+    log4j = sc._jvm.org.apache.log4j
+    log4j.LogManager.getRootLogger().setLevel(log4j.Level.ERROR)
+
+    # localhost:5555にソケット接続する
+    lines = ssc.socketTextStream("localhost", 5555)
+
+    # 取得したツイート情報を標準出力する
+    lines.pprint()
+
+    # ストリーム処理開始
+    ssc.start()
+    # Terminateされるまで待機
+    ssc.awaitTermination()
+
+
+if __name__ == "__main__":
+    main()
---
+++ twitter-pyspark-streaming-example/twitter_stream.py
@@ -0,0 +1,36 @@
+import tweepy
+from tweepy.auth import OAuthHandler
+from tweepy.streaming import StreamListener
+import socket
+
+import settings
+
+
+class MyStreamListener(StreamListener):
+
+    def on_status(self, status):
+        """on_status
+
+        :param status:
+        """
+        (conn, address) = s.accept()
+        print(status.text)
+        conn.send(bytes(status.text, 'UTF-8'))
+
+
+if __name__ == "__main__":
+
+    # spark streamingからのsocket接続をポート5555で受け付ける
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("localhost", 5555))
+    s.listen(5)
+
+    # TwitterAPIを利用してTwiter情報を取得する
+    auth = OAuthHandler(settings.consumer_key, settings.consumer_secret)
+    auth.set_access_token(settings.access_token, settings.access_secret)
+    api = tweepy.API(auth)
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+    # "python"という単語を含むもののみにフィルタする
+    myStream.filter(track=['python'])
